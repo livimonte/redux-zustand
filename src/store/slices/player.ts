8212415c5f0e80c320cea5a9168f62976f829efe
Livingstone Del Monte <livi.monte@gmail.com>
2024-03-22 09:46:10 -0300

    feat: add autoplay

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 const initialCourseData = {
   currentModuleIndex: 0,
@@ -25,6 +25,8 @@ const initialCourseData = {
           { id: '_shA5Xwe8_4', title: 'Redux X7', duration: '10:05' },
           { id: 'h5JA3wfuW1k', title: 'Redux X8', duration: '06:33' },
           { id: 'poQXNp9ItL4', title: 'Redux X9', duration: '09:12' },
+          { id: '_shA5Xwe8_4', title: 'Redux X10', duration: '13:45' },
+          { id: '93p3LxR9xfM', title: 'Redux X11', duration: '10:05' },
         ],
       },
     ],
@@ -35,12 +37,28 @@ const playerSlice = createSlice({
   name: 'player',
   initialState: initialCourseData,
   reducers: {
-    play: (state, action) => {
+    play: (state, action: PayloadAction<[number, number]>) => {
       state.currentModuleIndex = action.payload[0]
       state.currentLessonIndex = action.payload[1]
+    },
+    next: (state) => {
+      const nextLessonIndex = state.currentLessonIndex + 1
+      const nextLesson = state.course.modules[state.currentModuleIndex].lessons[nextLessonIndex]
+
+      if (nextLesson) {
+        state.currentLessonIndex = nextLessonIndex
+      } else {
+        const nextModuleIndex = state.currentModuleIndex + 1
+        const nextModule = state.course.modules[nextModuleIndex]
+
+        if (nextModule) {
+          state.currentModuleIndex = nextModuleIndex
+          state.currentLessonIndex = 0
+        }
+      }
     },
   },
 })
 
 export const player = playerSlice.reducer
-export const { play } = playerSlice.actions
+export const { play, next } = playerSlice.actions
