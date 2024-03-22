3f6d28b98b70104f5527114699a91e8374c7a667
Livingstone Del Monte <livi.monte@gmail.com>
2024-03-21 23:26:06 -0300

    feat: add video action click

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,25 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from '../store'
 
 export function Video() {
+  const lesson = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    const currentLesson =
+      state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+
+    return currentLesson
+  })
+
+  console.log(lesson)
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=hdex1hj1dmk"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,50 +1,46 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialCourseData = {
-  modules: [
-    {
-      id: '1',
-      title: 'Getting Started with React',
-      lessons: [
-        { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
-        { id: 'w-DW4DhDfcw', title: 'Post Styling', duration: '10:05' },
-        { id: 'D83-55LUdKE', title: 'Header Component', duration: '06:33' },
-        { id: 'W_ATsETujaY', title: 'Sidebar Component', duration: '09:12' },
-        { id: 'Pj8dPeameYo', title: 'Global CSS', duration: '03:23' },
-        { id: '8KBq2vhwbac', title: 'Comment Form', duration: '11:34' },
-      ],
-    },
-    {
-      id: '2',
-      title: 'Application Structure',
-      lessons: [
-        { id: 'gE48FQXRZ_o', title: 'Comment Component', duration: '13:45' },
-        { id: 'Ng_Vk4tBl0g', title: 'Responsiveness', duration: '10:05' },
-        { id: 'h5JA3wfuW1k', title: 'JSX Interactions', duration: '06:33' },
-        { id: '1G0vSTqWELg', title: 'Using State', duration: '09:12' },
-      ],
-    },
-    {
-      id: '3',
-      title: 'Understanding Redux',
-      lessons: [
-        { id: 'poQXNp9ItL4', title: 'Redux for Beginners', duration: '14:44' },
-        { id: 'CVpUuw9XSjY', title: 'Learn Redux in 20 Minutes', duration: '20:53' },
-        { id: '9zySeP5vH9c', title: 'Redux and React Hooks', duration: '18:28' },
-        { id: '93p3LxR9xfM', title: 'Redux Saga with API calls', duration: '23:16' },
-        { id: 'k68HXdNU2oM', title: 'Redux Thunk Explained', duration: '10:45' },
-        { id: 'xcLZpRncLZQ', title: 'Advanced Redux Patterns', duration: '15:02' },
-      ],
-    },
-  ],
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+  course: {
+    modules: [
+      {
+        id: '1',
+        title: 'Getting Started with Redux',
+        lessons: [
+          { id: '_shA5Xwe8_4', title: 'Redux in 100 Seconds', duration: '13:45' },
+          { id: '93p3LxR9xfM', title: 'Redux X', duration: '10:05' },
+          { id: 'poQXNp9ItL4', title: 'Redux X2', duration: '06:33' },
+          { id: 'W_ATsETujaY', title: 'Redux X3', duration: '09:12' },
+          { id: '_shA5Xwe8_4', title: 'Redux X4', duration: '03:23' },
+          { id: '8KBq2vhwbac', title: 'Redux X5', duration: '11:34' },
+        ],
+      },
+      {
+        id: '2',
+        title: 'Application Structure',
+        lessons: [
+          { id: 'gE48FQXRZ_o', title: 'Redux X6', duration: '13:45' },
+          { id: '_shA5Xwe8_4', title: 'Redux X7', duration: '10:05' },
+          { id: 'h5JA3wfuW1k', title: 'Redux X8', duration: '06:33' },
+          { id: 'poQXNp9ItL4', title: 'Redux X9', duration: '09:12' },
+        ],
+      },
+    ],
+  },
 }
 
 const playerSlice = createSlice({
   name: 'player',
-  initialState: {
-    course: initialCourseData,
+  initialState: initialCourseData,
+  reducers: {
+    play: (state, action) => {
+      state.currentModuleIndex = action.payload[0]
+      state.currentLessonIndex = action.payload[1]
+    },
   },
-  reducers: {},
 })
 
 export const player = playerSlice.reducer
+export const { play } = playerSlice.actions
