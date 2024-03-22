94ef9edd3fb1944ca201406b1521956203929e1b
Livingstone Del Monte <livi.monte@gmail.com>
2024-03-22 11:16:24 -0300

    feat: add hook for selector

---
+++ src/store/hooks.ts
@@ -0,0 +1,12 @@
+import { useAppSelector } from '.'
+
+export const useCurrentLesson = () => {
+  return useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    const currentModule = state.player.course.modules[currentModuleIndex]
+    const currentLesson = currentModule.lessons[currentLessonIndex]
+
+    return { currentModule, currentLesson }
+  })
+}
